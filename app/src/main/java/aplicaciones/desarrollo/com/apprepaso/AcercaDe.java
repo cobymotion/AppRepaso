7015d48f10da90bfb4cfd69e9a8a0ea409094841
Luis Cobian <coby10@gmail.com>
2015-04-15 12:00:20 -0500

    Ya manda a la actividad de Acerca De

--- app/src/main/java/aplicaciones/desarrollo/com/apprepaso/AcercaDe.java
+++ app/src/main/java/aplicaciones/desarrollo/com/apprepaso/AcercaDe.java
@@ -1,32 +1,24 @@
 package aplicaciones.desarrollo.com.apprepaso;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class AcercaDe extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.salir).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, AcercaDe.class));
-            }
-        });
+        setContentView(R.layout.activity_acerca_de);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_acerca_de, menu);
         return true;
     }
 
